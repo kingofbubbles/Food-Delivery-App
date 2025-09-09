a04a9ea9684b85db36d8f9aa18f41a8050a7aaf3
Joshua Chuang <joshthegreatsayshi@gmail.com>
2025-09-09 14:23:44 -0400

    Config setup to begin home screen, assets added and loaded

--- fast_food/app/index.tsx
+++ fast_food/app/index.tsx
@@ -4,8 +4,8 @@ import './globals.css';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+        Welcome to my React Native App!
       </Text>
     </View>
   );
